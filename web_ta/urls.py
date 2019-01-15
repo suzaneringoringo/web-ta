438ac7482e215120af222d472895a72e886f86f5
Suzane Ringoringo <suzaneringoringo@gmail.com>
2019-01-15 18:15:02 +0700

    hello world

--- web_ta/urls.py
+++ web_ta/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('make_data/', include('make_data.urls')),
     path('admin/', admin.site.urls),
 ]
